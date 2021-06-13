aa611730ff9caf7f0a71b86b861c2e2af8827987
vemv <vemv@users.noreply.github.com>
2021-06-13 05:04:11 +0200

    try

--- .circleci/nvd/project.clj
+++ .circleci/nvd/project.clj
@@ -2,7 +2,7 @@
   :description "Meant for analyzing other projects' classpaths in an isolated manner,
 so that lein-nvd's dependencies don't affect the analyzed corpus (leading to false positives/negatives) OR
 the target project's dependencies affect lein-nvd's implementation (leading to bugs)."
-  :dependencies [[nvd-clojure "1.5.0"]
+  :dependencies [[nvd-clojure "1.5.0" #_"NOTE: if updating this dependency, please make sure `~/.m2/repository/org/owasp/dependency-check-utils/6.2.2/` in config.yml remains valid"]
                  [org.clojure/clojure "1.10.3"]]
   :jvm-opts ["-Dclojure.main.report=stderr"]
   :pedantic? :abort)
